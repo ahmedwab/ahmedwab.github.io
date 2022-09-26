2cfa06913b60c51f9357c9f60a7313e62c7abadc
ahmedwab <a.abdelfattah@vehikl.com>
2022-09-26 13:19:09 -0400

    Update social links

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,8 +4,12 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 const Experience = () => {
   return (
     <div className="socials" id="socials">
-      <AiFillLinkedin size={30} href="https://linkedin.com/in/ahmedwab" cursor={'pointer'}/>
-      <AiFillGithub size={30} href="https://github.com/ahmedwab" cursor={'pointer'}/>
+      <AiFillLinkedin size={30} onClick={() => {
+          window.location.href = "https://www.linkedin.com/in/ahmedwab"
+      }} cursor={'pointer'}/>
+       <AiFillGithub size={30} onClick={() => {
+          window.location.href = "https://www.github.com/ahmedwab"
+      }} cursor={'pointer'}/>
     </div>
   );
 }
